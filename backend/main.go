4617f559ed62d295a0b77b9cb6df601bb4de3ecd
Raju kadel <rajukadel.github@gmail.com>
2024-06-21 22:30:32 +0545

    setup routes

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "net/http"
+import (
+	customWebsocket "chatapp/websocket"
+	"fmt"
+	"net/http"
+)
+
+func serveWs(pool *customWebsocket.Pool, w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("This websocket server is working properly...")
+
+	conn, err := customWebsocket.Upgrade(w, r)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	client := &customWebsocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
+}
+
+func setupRoutes() {
+
+	fmt.Println("Setup routes is called() ")
+
+	pool := customWebsocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+
+}
 
 func main() {
+	setupRoutes()
 	http.ListenAndServe(":9000", nil)
 }
